22761e05a0e48527b86ec50b7089b710ff578897
Miguel Barceló <miguel.barcelo@gmail.com>
2023-03-05 21:02:46 -0700

    feat: Added Professor Controller

--- src/controllers/professorController.js
+++ src/controllers/professorController.js
@@ -1,23 +1,70 @@
 const Professors = require('../models/Professors');
 
-const getProfessors = (req, res) => {
-  throw Error('ProfessorController.getProfessors not implemented!');
+const getProfessors = async (req, res) => {
+  try {
+    const professors = await Professors.find({});
+    return res.json(professors);
+  } catch (err) {
+    console.error('Error getting professors:', err);
+    res.status(500).json({ msg: 'Error getting professor' });
+  }
 };
 
-const getProfessor = (req, res) => {
-  throw Error('ProfessorController.getProfessor not implemented!');
+const getProfessor = async (req, res) => {
+  const { id: _id } = req.params;
+
+  try {
+    const professor = await Professors.findById(_id);
+    if (!professor) {
+      return res.status(404).json({ msg: 'Professor not found' });
+    }
+    return res.json(professor);
+
+  } catch (err) {
+    console.error('Error getting professor:', err);
+    return res.status(500).json({ msg: 'Error getting professor' });
+  }
 };
 
-const createProfessor = (req, res) => {
-  throw Error('ProfessorController.createProfessor not implemented!');
+const createProfessor = async (req, res) => {
+  const { body } = req;
+
+  try {
+    const professor = await Professors.create(body);
+    return res.json(professor);
+  } catch (err) {
+    console.error('Error creating professor:', err);
+    return res.status(500).json({ msg: 'Error creating professor' });
+  }
 };
 
-const updateProfessor = (req, res) => {
-  throw Error('ProfessorController.updateProfessor not implemented!');
+const updateProfessor = async (req, res) => {
+  const { id: _id } = req.params;
+  const { body } = req;
+
+  try {
+    let professor = await Professors.findById(_id);
+    if (!professor) {
+      return res.status(404).json({ msg: 'Professor not found' });
+    }
+    professor = await Professors.findByIdAndUpdate(_id, body, { new: true });
+    return res.json({ professor });
+  } catch (err) {
+    console.error('Error updating professor:', err);
+    return res.status(500).json({ msg: 'Error updating professor' });
+  }
 };
 
-const deleteProfessor = (req, res) => {
-  throw Error('ProfessorController.deleteProfessor not implemented!');
+const deleteProfessor = async (req, res) => {
+  const { id: _id } = req.params;
+
+  try {
+    await Professors.findByIdAndDelete(_id)
+    return res.json({ msg: 'Professor deleted' });
+  } catch (err) { 
+    console.error('Error deleting professor:', err);
+    return res.status(500).json({ msg: 'Error deleting professor' });
+  }
 };
 
 module.exports = {
--- src/routes/professors.routes.js
+++ src/routes/professors.routes.js
@@ -3,14 +3,14 @@ const professorController = require('../controllers/professorController')
 
 const router = Router();
 
-router.get('/professors', professorController.getProfessors);
+router.get('/', professorController.getProfessors);
 
-router.get('/professors/:id', professorController.getProfessor);
+router.get('/:id', professorController.getProfessor);
 
-router.post('/professors', professorController.createProfessor);
+router.post('/', professorController.createProfessor);
 
-router.put('/professors/:id', professorController.updateProfessor);
+router.put('/:id', professorController.updateProfessor);
 
-router.delete('/professors/:id', professorController.deleteProfessor);
+router.delete('/:id', professorController.deleteProfessor);
 
 module.exports = router;
